5a6f5d6f0a8b9b7ed66835e551543cb11294a9fc
stevleibelt <stev@leibelt.de>
2013-07-01 22:42:18 +0200

    Added alias interface

---
+++ source/Net/Bazzline/Component/ServiceLocator/AliasInterface.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-01 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+/**
+ * Class AliasInterface
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-01
+ */
+interface AliasInterface
+{
+    /**
+     * @return string
+     * @throws \InvalidArgumentException - if no alias is set
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function getAlias();
+
+    /**
+     * Validates if the instance has an alias
+     *
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function hasAlias();
+
+    /**
+     * Sets alias
+     *
+     * @return $this
+     * @throws \RuntimeException - if you want to set an alias the second time
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function setAlias();
+}
