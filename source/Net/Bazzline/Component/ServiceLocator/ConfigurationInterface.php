6978c6227d1952c982daabe7f2d1a5f8673ce905
stevleibelt <stev@leibelt.de>
2013-07-02 00:05:48 +0200

    First idea of configuration interface

---
+++ source/Net/Bazzline/Component/ServiceLocator/ConfigurationInterface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-02 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+/**
+ * Class ConfigurationInterface
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-01
+ */
+interface ConfigurationInterface
+{
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function load();
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function compile();
+
+    /**
+     * Dumps compiled configuration to a php file content stream.
+     *
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-01
+     */
+    public function dump();
+}
