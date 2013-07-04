d2df6c5f0a90851b7056ba7ebc6604d8d0fc7fe5
stevleibelt <stev@leibelt.de>
2013-07-04 23:53:54 +0200

    Created builderdefinition interface

---
+++ source/Net/Bazzline/Component/ServiceLocator/BuilderDefinitionInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-04 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+/**
+ * Class BuilderDefinitionInterface
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-04
+ */
+interface BuilderDefinitionInterface
+{
+
+}
