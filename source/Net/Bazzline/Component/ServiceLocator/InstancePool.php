583de1d8d6510977a9e14a6a604018e6453db674
stevleibelt <stev@leibelt.de>
2013-07-04 23:51:27 +0200

    Added instancepool

---
+++ source/Net/Bazzline/Component/ServiceLocator/InstancePool.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-04 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+use SplObjectStorage;
+
+/**
+ * Class InstancePool
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-04
+ */
+class InstancePool extends SplObjectStorage {}
