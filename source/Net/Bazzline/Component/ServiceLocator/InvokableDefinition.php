10c1464a8e5558e687f97e1ef59b989b1fd03731
stevleibelt <stev@leibelt.de>
2013-07-05 23:59:20 +0200

    Created InvokableDefinition and Interface

---
+++ source/Net/Bazzline/Component/ServiceLocator/InvokableDefinition.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-05 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+/**
+ * Class InvokableDefinition
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-05
+ */
+class InvokableDefinition extends Definition implements InvokableDefinitionInterface {}
---
+++ source/Net/Bazzline/Component/ServiceLocator/InvokableDefinitionInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-05 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator;
+
+/**
+ * Class InvokableDefinitionInterface
+ *
+ * @package Net\Bazzline\Component\ServiceLocator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-05
+ */
+interface InvokableDefinitionInterface extends AliasableInterface, NameableInterface {}
