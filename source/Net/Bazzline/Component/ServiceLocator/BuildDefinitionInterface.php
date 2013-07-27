aa7bbcd89bcccc8af6f6aaf1d8b6c607842a471e
stevleibelt <stev@leibelt.de>
2013-07-28 00:38:11 +0200

    Adapted builder definition

--- source/Net/Bazzline/Component/ServiceLocator/BuildDefinitionInterface.php
+++ source/Net/Bazzline/Component/ServiceLocator/BuildDefinitionInterface.php
@@ -15,5 +15,23 @@ namespace Net\Bazzline\Component\ServiceLocator;
  */
 interface BuildDefinitionInterface
 {
+    public function setDependency($classNameOrAlias, $isRequired = true);
 
+    public function hasDependency($classNameOrAlias);
+
+    public function isDependencyRequired($name);
+
+    public function getDependency($name);
+
+    public function getDependencies();
+
+    public function setParameter($name, $value, $isRequired = true);
+
+    public function hasParameter($name);
+
+    public function isParameterRequired($name);
+
+    public function getParameter($name);
+
+    public function getParameters();
 }
