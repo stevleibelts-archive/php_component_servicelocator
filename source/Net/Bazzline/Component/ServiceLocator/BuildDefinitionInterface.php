02f57ad0809a00fe5366d018a3254d4105a26644
stevleibelt <stev@leibelt.de>
2013-07-28 02:18:46 +0200

    Updaded interface

--- source/Net/Bazzline/Component/ServiceLocator/BuildDefinitionInterface.php
+++ source/Net/Bazzline/Component/ServiceLocator/BuildDefinitionInterface.php
@@ -15,23 +15,67 @@ namespace Net\Bazzline\Component\ServiceLocator;
  */
 interface BuildDefinitionInterface
 {
+    /**
+     * @param DependencyInterface $dependency
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function setDependency(DependencyInterface $dependency);
 
-    public function hasDependency($classNameOrAlias);
+    /**
+     * @param string $name
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
+    public function hasDependency($name);
 
-    public function isDependencyRequired($name);
-
+    /**
+     * @param string $name
+     * @return DependencyInterface
+     * @throws \InvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function getDependency($name);
 
+    /**
+     * @return array|DependencyInterface[]
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function getDependencies();
 
+    /**
+     * @param ParameterInterface $parameter
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function setParameter(ParameterInterface $parameter);
 
+    /**
+     * @param string $name
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function hasParameter($name);
 
-    public function isParameterRequired($name);
-
+    /**
+     * @param string $name
+     * @return ParameterInterface
+     * @throws \InvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function getParameter($name);
 
+    /**
+     * @return array|ParameterInterface[]
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-28
+     */
     public function getParameters();
 }
