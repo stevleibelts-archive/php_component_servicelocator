22789dddd6f0e7db894a6569d315a76cac6e016f
stevleibelt <stev@leibelt.de>
2013-07-28 02:04:39 +0200

    Renamed

--- source/Net/Bazzline/Component/ServiceLocator/Dependency/ConstructorDependencyInterface.php
+++ source/Net/Bazzline/Component/ServiceLocator/Dependency/ConstructorDependencyInterface.php
@@ -9,10 +9,10 @@ namespace Net\Bazzline\Component\ServiceLocator\Dependency;
 use Net\Bazzline\Component\ServiceLocator\PositionableInterface;
 
 /**
- * Class ConstructorInterface
+ * Class ConstructorDependencyInterface
  *
  * @package Net\Bazzline\Component\ServiceLocator\Dependency
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-07-28
  */
-interface ConstructorInterface extends DependencyInterface, PositionableInterface {}
+interface ConstructorDependencyInterface extends DependencyInterface, PositionableInterface {}
