57905ed9642b26c6021946536046c18060ef48e8
stevleibelt <stev@leibelt.de>
2013-07-30 23:38:41 +0200

    Fixed bug

---
+++ source/Net/Bazzline/Component/ServiceLocator/DependencyInjection/ParameterInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-28 
+ */
+
+namespace Net\Bazzline\Component\ServiceLocator\DependencyInjection;
+
+class ParameterInterface
+{
+
+}
